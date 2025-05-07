55db7337aeec7d442c1fbce952093e2574d10a17
Zhanna1582 <Zhanna1582@i.ua>
2025-05-07 20:14:31 +0300

    Виправлено помилку з токеном у bot.py

--- bot.py
+++ bot.py
@@ -242,7 +242,7 @@ def main():
         send_daily_fact,
         'cron',
         hour=20,
-        minute=2,  # Исправлено значение минуты
+        minute=18,  # Исправлено значение минуты
         timezone=kyiv_tz,
         args=[bot]  # Pass bot instance to the job
     )
